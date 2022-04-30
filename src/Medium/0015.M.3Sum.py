6717e23e4c65e6f43455aaba1d2c278f40345824
Logan (Ligang) Chen <ligang.chen@gmail.com>
2022-04-29 21:25:51 -0400

    Update 0015.M.3Sum.py

--- src/Medium/0015.M.3Sum.py
+++ src/Medium/0015.M.3Sum.py
@@ -1,11 +1,10 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         """
-        : type nums: List[int]
-        : rtype: List[List[int]]
-        :
+        :\Algo. 1.
         : TC: NA, O(n^3)
         : SC: NA, O(n) for res_SET
+        :
         :\BF algo., 3-loop, use set of tuple to avoid duplicate triplets.
         : result: 315 / 318 test cases passed. Status: Time Limit Exceeded
         """
@@ -28,11 +27,10 @@ class Solution:
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         """
-        : type nums: List[int]
-        : rtype: List[List[int]]
-        :
+        :\Algo. 2.
         : TC: 97.17%, O(n*logn) for sorting, the rest should be O(n).
         : SC: 15.09%, O(n), for sorted list, dicts set_res etc.
+        :
         :\optimized algo. 3 cases: 3 zeros easy; 1 zero relatively easy; no zero, then similar to 2sum, can use dict; 
         : two pointers (normally) with sort;
         :\There could be several senarios for duplicates, e.g. [-6, 4, 2] and [-6, 2, 4], [-6, 3, 3], use set 
@@ -104,3 +102,81 @@ class Solution:
                 idx_neg += 1
         
         return list(set_res)
+
+    
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        """
+        :\Algo. 3.
+        : TC:5.01%, O(n^2), 2-loop, (O(n*logn) for sorting.)
+        : SC: 99.22%, O(n), for sorted list, dicts set_res etc.
+        :
+        :\not to consider 3 zeros or no zeros cases, sorted array, fix one-element, then two pointers with sorted array, 
+        : be careful about how to move the two-pointer. 
+        """
+        n = len(nums)
+        if n <= 2: 
+            return []
+        
+        nums_sorted = sorted(nums)   
+        set_res = set()
+        
+        for curr_idx in range(n): 
+            twosum_target = -nums_sorted[curr_idx]
+            low, high = 0, n-1
+            while low < high:
+                if low == curr_idx: 
+                    low += 1
+                    continue
+                if high == curr_idx:
+                    high -= 1
+                    continue
+                    
+                if nums_sorted[low] + nums_sorted[high] == twosum_target:
+                    set_res.add(tuple(sorted([nums_sorted[curr_idx], nums_sorted[low], nums_sorted[high]])))
+                    low += 1
+                    high -= 1
+                elif nums_sorted[low] + nums_sorted[high] < twosum_target:
+                    low += 1
+                else:
+                    high -= 1
+        
+        return list(set_res)
+    
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        """
+        :\Algo. 4.
+        : TC: 5.01%, O(n^2), 2-loop
+        : SC: 23.33%, O(n), for set_res etc.
+        : Another run result:
+        : 317 / 318 test cases passed.
+        : Status: Time Limit Exceeded
+        :
+        :\not to consider 3 zeros or no zeros cases, not to use sorted order and two-pointer, fix one number, 
+        : then use hash table (i.e. dict, set) to solve the two-sum problem. Use set or sorted tuples to avoid 
+        : duplicate results.
+        """
+            
+        n = len(nums)
+        if n <= 2: 
+            return []
+        
+        set_res = set()
+        
+        for curr_idx in range(n):
+            twosum_target = -nums[curr_idx]
+            set_existed = set()
+            for i in range(n):
+                if i==curr_idx: 
+                    continue
+                
+                if twosum_target - nums[i] not in set_existed:
+                    set_existed.add(nums[i])
+                else:
+                    set_res.add(tuple(sorted([nums[curr_idx], nums[i], twosum_target-nums[i]])))
+        
+        return list(set_res)
+    
+    
