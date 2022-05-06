e1cc581805ec5139a810f2a6efd6f974683b0de9
Logan (Ligang) Chen <ligang.chen@gmail.com>
2022-05-06 13:58:00 -0400

    Update 0007.M.ReverseInteger.py

--- src/Medium/0007.M.ReverseInteger.py
+++ src/Medium/0007.M.ReverseInteger.py
@@ -5,8 +5,8 @@ class Solution:
         : and floor division operation. check overflow/underflow. In solution 1, it uses negative 
         : value to get the remainder, here I converted x to positive first. Be aware -3%10 = 7, 
         : it's different, need to make sure it's ok. 
-        : TC: O(1) or O(logx), 22.62%
-        : SC: O(1), 16.59%
+        : TC: O(1) or O(logx), 97.07%
+        : SC: O(1), 97.42%
         '''
         
         sign = 1 if x >= 0 else -1
@@ -14,12 +14,13 @@ class Solution:
         result = 0
         
         INT_MAX = 2**31 - 1
-        INT_MIN = -2**31
         
         while x > 0:
             digit = x%10
             x = (x-digit)//10  # not / or it becomes float!
-            if result > INT_MAX//10 or (result==INT_MAX//10 and digit > INT_MAX%10):
+            #\below the "or" condition is not necessary, if result==INT_MAX//10 => 214748364
+            # then the original number is 463847412 plus one more digit, which is impossible. 
+            if result > INT_MAX//10:  # or (result==INT_MAX//10 and digit > INT_MAX%10):
                 return 0
             result = 10*result + digit
         
