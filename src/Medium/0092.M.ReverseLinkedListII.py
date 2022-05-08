938ead7b5c4c94f90910dbad7e58fb2ac42cee31
Logan (Ligang) Chen <ligang.chen@gmail.com>
2022-05-08 15:52:56 -0400

    Update 0092.M.ReverseLinkedListII.py

--- src/Medium/0092.M.ReverseLinkedListII.py
+++ src/Medium/0092.M.ReverseLinkedListII.py
@@ -1,3 +1,8 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
         '''
@@ -39,6 +44,49 @@ class Solution:
             head = prev
         
         pl.next = right_next
+        
+        return head
+    
+    
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        '''
+        :\Algo. 2, iteration with 1-pass while-loop, be aware need to record the "left_prev" node, 
+        : which will be used eventually; and need to keep the "next" pointer first. 
+        : TC: O(n), 74.51%, 
+        : SC: O(1), 51.54%, 
+        '''
+        
+        if left==right: return head
+        
+        prev, curr = None, head
+        idx = 1
+        while idx <= right:
+            if idx==left:
+                left_prev = prev
+                prev_rvs = None
+            
+            if idx >= left and idx < right:
+                next = curr.next
+                curr.next = prev_rvs
+                prev_rvs = curr
+            elif idx == right:
+                next = curr.next
+                curr.next = prev_rvs
+                prev_rvs = curr
+                
+                if left_prev:
+                    left_prev.next.next = next
+                    left_prev.next = prev_rvs
+                else:
+                    head.next = next
+                    head = prev_rvs
+            else:
+                next = curr.next
+            
+            prev = curr
+            curr = next
+            idx += 1
         
         return head
     
