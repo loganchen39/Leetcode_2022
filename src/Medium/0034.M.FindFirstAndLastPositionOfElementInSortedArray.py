5a52e506716d9258a483578683f86e85a5f053d3
Logan (Ligang) Chen <ligang.chen@gmail.com>
2022-05-12 15:15:37 -0400

    Update 0034.M.FindFirstAndLastPositionOfElementInSortedArray.py

--- src/Medium/0034.M.FindFirstAndLastPositionOfElementInSortedArray.py
+++ src/Medium/0034.M.FindFirstAndLastPositionOfElementInSortedArray.py
@@ -30,3 +30,39 @@ class Solution:
         return [-1, -1]
     
     
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        '''
+        :\Algo. 2, similar to Algo. 1, now mofify the "return" criterion to find the left/right bound.
+        : TC: O(logn), 83.96%
+        : SC: O(1), 51.50%
+        '''
+            
+        left = self.findBound(nums, target, True)
+        right = self.findBound(nums, target, False)
+        return [left, right]
+        
+    def findBound(self, nums: List[int], target: int, isFirst: bool) -> int:
+        n = len(nums)
+        lo, hi = 0, n-1
+        while lo<=hi:
+            mid = int((lo+hi)/2)
+            if nums[mid] == target:
+                if isFirst:
+                    if mid==0 or nums[mid-1] != target:
+                        return mid
+                    else:
+                        hi = mid-1
+                else:
+                    if mid==n-1 or nums[mid+1] != target:
+                        return mid
+                    else:
+                        lo = mid+1
+            elif target < nums[mid]:
+                hi = mid-1
+            else:
+                lo = mid+1
+        
+        return -1
+    
+    
