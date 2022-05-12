6efcad4525fec37a6c1484b6f15894872056e568
Logan (Ligang) Chen <ligang.chen@gmail.com>
2022-05-12 12:37:39 -0400

    Update 0702.M.SearchInASortedArrayOfUnknownSize.py

--- src/Medium/0702.M.SearchInASortedArrayOfUnknownSize.py
+++ src/Medium/0702.M.SearchInASortedArrayOfUnknownSize.py
@@ -7,6 +7,45 @@
 
 class Solution:
     def search(self, reader: 'ArrayReader', target: int) -> int:
+        '''
+        :\Algo. 1, find the section by right*=2 until it's out of the array, then 2 scenarios with binary search.
+        : TC: O(logn), 27.96%
+        : SC: O(1), 96.34%
+        '''
         
+        left, right = 0, 1
+        num_right = reader.get(right)
+        while num_right < 10**5 and target > num_right:
+            left = right+1
+            right *= 2
+            num_right = reader.get(right)
+        
+        if num_right > 10**5: # check last section
+            lo, hi = left, right-1
+            while lo <= hi:
+                mid = int((lo+hi)/2)
+                num_right = reader.get(mid)
+                if num_right > 10**5:
+                    hi = mid-1
+                elif num_right == target:
+                    return mid
+                elif target < num_right:
+                    hi = mid-1
+                else:
+                    lo = mid+1
+                    
+            return -1
+        elif target <= num_right:  # use binary search
+            lo, hi = left, right
+            while lo <= hi:
+                mid = int((lo+hi)/2)
+                if reader.get(mid) == target:
+                    return mid
+                elif target < reader.get(mid):
+                    hi = mid-1
+                else:
+                    lo = mid+1
+            
+            return -1
         
         
