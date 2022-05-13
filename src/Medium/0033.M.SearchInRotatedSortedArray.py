c876047f05f319e507552c786a955740f89965b4
Logan (Ligang) Chen <ligang.chen@gmail.com>
2022-05-13 10:50:57 -0400

    Update 0033.M.SearchInRotatedSortedArray.py

--- src/Medium/0033.M.SearchInRotatedSortedArray.py
+++ src/Medium/0033.M.SearchInRotatedSortedArray.py
@@ -34,3 +34,56 @@ class Solution:
         return -1
     
     
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        '''
+        :\Algo. 2, inspired from approach 1, use binary search to find the pivot index, 
+        : the key information here is that, a) all elements are unique, b), you can determine 
+        : which segment an element lie in (the 1st or 2nd) by comparing with the nums[0], c) pay 
+        : attention to the special cases of pivot at 0, i.e. normal increasing array without pivot.
+        : TC: O(logn), 61.90%
+        : SC: O(1), 57.78%
+        '''
+        
+        def find_pivot_index(left, right):
+            if left == right:
+                return left
+            
+            if nums[left] <= nums[right]:
+                return left
+            
+            while left <= right:
+                pivot = (left+right) // 2
+                if nums[pivot] > nums[pivot+1]:
+                    return pivot+1
+                else:
+                    if nums[pivot] >= nums[left]:
+                        left = pivot + 1
+                    else:
+                        right = pivot - 1
+        
+        
+        n = len(nums)
+        
+        pivot = find_pivot_index(0, n-1)
+        if target >= nums[0]:
+            lo = 0
+            if pivot == 0:  # special case
+                hi = n - 1
+            else:
+                hi = pivot - 1
+        else:
+            lo, hi = pivot, n-1
+        
+        while lo <= hi:
+            mid = (lo+hi)//2
+            if target == nums[mid]:
+                return mid
+            elif target < nums[mid]:
+                hi = mid - 1
+            else:
+                lo = mid + 1
+        
+        return -1
+    
+    
