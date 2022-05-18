ff876611f2586c41c4b8dea5d75fde2b2a131a0e
Logan (Ligang) Chen <ligang.chen@gmail.com>
2022-05-18 10:37:54 -0400

    Update 0104.E.MaximumDepthOfBinaryTree.py

--- src/Easy/0104.E.MaximumDepthOfBinaryTree.py
+++ src/Easy/0104.E.MaximumDepthOfBinaryTree.py
@@ -17,3 +17,33 @@ class Solution:
         return 1+max(self.maxDepth(root.left), self.maxDepth(root.right))
     
     
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        '''
+        :\Algo. 2, iteration with explicit stack, the key point here is to attach and record the 
+        : depth to each node in the stack, i.e. stack = [[root, 1]], then update. 
+        : TC: O(n), 40.54%, n being the total number of nodes, 
+        : SC: O(m), 81.17%, m being the number of right-child nodes that are temporarily in the stack, 
+        : worst case n/2 when each left-child node has both a left and right-child, while each right-child 
+        : does not have any child;, best case O(1) when each tree node does not have left and right-child simultaneously.
+        '''
+        
+        if not root:
+            return 0
+        
+        stack = [[root, 1]]
+        maxD = 1
+        while stack:
+            pt, d = stack.pop()
+            if pt.right:
+                stack.append([pt.right, d+1])
+                if d+1 > maxD:
+                    maxD = d+1
+            if pt.left:
+                stack.append([pt.left, d+1])
+                if d+1 > maxD:
+                    maxD = d+1
+
+        return maxD
+    
+    
