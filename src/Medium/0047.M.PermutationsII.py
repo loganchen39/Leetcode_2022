36bd6c2396df1da67d30afe02d40c0f30ea5f479
Logan (Ligang) Chen <ligang.chen@gmail.com>
2022-05-30 13:47:49 -0400

    Update 0047.M.PermutationsII.py

--- src/Medium/0047.M.PermutationsII.py
+++ src/Medium/0047.M.PermutationsII.py
@@ -33,3 +33,34 @@ class Solution:
         return list(results)
     
     
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        '''
+        :\Algo. 2, backtracking, here the key point which is also the reason why Algo. 1 failed, 
+        : is that we need to find the permutation of the indices i, not the numbers nums[i], 
+        : due to possible duplicate numbers in nums, and then use set to avoid duplicate. 
+        : TC: O(), 17.13%
+        : SC: O(), 22.73%
+        '''
+        
+        results = set()
+        n = len(nums)
+        
+        def backtrack(perm, idx):
+            if idx == n:
+                results.add(tuple([nums[i] for i in perm]))
+                return
+            
+            for i in range(n):
+                if i in perm:
+                    continue
+                #perm.append(nums[i])
+                perm.append(i)
+                backtrack(perm, idx+1)
+                perm.pop()
+        
+        
+        backtrack([], 0)
+        return list(results)
+    
+    
